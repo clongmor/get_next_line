457c44d2b293cf33f1ed748b650e6a431aa4a138
Chloe LONGMORE <clongmor@c3r4s6.wethinkcode.co.za>
2019-06-12 08:28:56 +0200

    basics done

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clongmor <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/11 09:32:37 by clongmor          #+#    #+#             */
+/*   Updated: 2019/06/11 11:25:27 by clongmor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+#  define BUFF_SIZE 32
+#include <stdlib.h>
+#include "/libft/libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#endif
